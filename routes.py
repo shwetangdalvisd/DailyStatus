20e3f6745b0279bf7decb5499fc29c5a0424a93c
shwetangdalvisd <64190474+shwetangdalvisd@users.noreply.github.com>
2020-04-23 13:35:00 +0530

    Delete routes.py

--- routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, url_for, flash, redirect, request,session
-from daily import app, mongo, bcrypt
-from dailystatus.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from flask_login import login_user, current_user, logout_user, login_required
-
-@app.route("/")
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    return render_template('login.html', title='Login', form=form)
