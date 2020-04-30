506dc6220d72d10a26192fc3efe57f987c4e84d7
shwetangdalvisd <64190474+shwetangdalvisd@users.noreply.github.com>
2020-04-30 20:04:13 +0530

    new 30 april

--- dailystatus/forms.py
+++ dailystatus/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectMultipleField, DateField,SelectField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectMultipleField, DateField,SelectField,TextField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Optional
 from dailystatus import app, mongo
-
+from flask_login import login_user, current_user
 
 class LoginForm(FlaskForm):
 	email = StringField('Email',validators=[DataRequired(), Email()])
@@ -16,13 +16,13 @@ class RegisterForm(FlaskForm):
 	email = StringField('Email',validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
-	role = StringField('Role',validators=[DataRequired()])
+	role = SelectField('Role',validators=[DataRequired()])
 	submit = SubmitField('Sign Up')
 
 class ProjectForm(FlaskForm):
 	Project = StringField('Project Name',validators=[DataRequired()])
 	doc = StringField('Date of Completion')
-	Cromail = StringField('Email',validators=[DataRequired(), Email()])
+	Cromail = StringField('CRO Email',validators=[Optional(), Email()])
 	submit = SubmitField('submit')
 
 def usf():
@@ -43,7 +43,27 @@ class AssignForms(FlaskForm):
 	project = SelectField('project',choices=pjf())
 	projectteam = SelectField('projectteam')
 	submit = SubmitField('submit')
- 
+
+class StatusForm(FlaskForm):
+	project = SelectField('Project',validators=[DataRequired()])
+	date = DateField('Date',format='%d/%m/%Y',validators=[DataRequired()])
+	jirano = StringField('Jira NO.',validators=[DataRequired()])
+	desc = TextField('Description',validators=[DataRequired()])
+	status = SelectField('username',validators=[DataRequired()])
+	env = SelectField('Env',validators=[DataRequired()])
+	comments = TextField('Comment', validators=[DataRequired()])
+	submit = SubmitField('submit')
+
+class View_statusForm(FlaskForm):
+	project = SelectField('Project', choices=pjf())
+	username = SelectField('Username',choices=usf())
+	date = StringField('Date')
+	submit = SubmitField('submit')
+
+class DeleteForms(FlaskForm):
+	username = SelectMultipleField('username',choices=usf(),validators=[Optional()])
+	project = SelectField('project',choices=pjf(),validators=[Optional()])
+	submit = SubmitField('submit')
 
 
 
--- dailystatus/routes.py
+++ dailystatus/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request,session,jsonify
 from dailystatus import app, mongo, bcrypt,login_manager
 from bson.json_util import dumps
-from dailystatus.forms import LoginForm,RegisterForm,ProjectForm,AssignForms
+from dailystatus.forms import LoginForm,RegisterForm,ProjectForm,AssignForms,StatusForm,DeleteForms,View_statusForm
 from flask_login import login_user, current_user, logout_user, login_required,UserMixin
 from bson.objectid import ObjectId
 import json
@@ -13,48 +13,48 @@ class JSONEncoder(json.JSONEncoder):
 		return json.JSONEncoder.default(self, o)
 
 class User(UserMixin):
-	def __init__(self, user_id):
-		self.user_id = user_id
+	def __init__(self, email):
+		self.email = email
 	def get_id(self):
-		return (self.user_id)
-
+		return self.email
 
 @login_manager.user_loader
-def load_user(user_id):
-	u = mongo.db.Users.find_one({"user_id": user_id})
+def load_user(email):
+	u = mongo.db.user.find_one({"mail_id" : email })
 	if not u:
 		return None
-	return User(user_id=u['user_id'])
+	return User(email=u['mail_id'])
 
 @app.route("/")
 @app.route("/login", methods=['GET', 'POST'])
 def login():
 	if current_user.is_authenticated:
-		return redirect(url_for('Register'))
+		return redirect(url_for('logout'))
 	form = LoginForm()
 	Uer = mongo.db.user
 	if form.validate_on_submit():
 		user = Uer.find_one({"mail_id": form.email.data})
 		if user is not None and bcrypt.check_password_hash(user['password'], form.password.data):
-			sd = JSONEncoder().encode(user)
-			user_obj = User(sd)
+			user_obj = User(email=user['mail_id'])
 			login_user(user_obj)
+			next_page = request.args.get('next')
 			if user['role'] == "admin":
-				return redirect(url_for('Register'))
+				return (redirect(url_for('Register')) if next_page else redirect(url_for('Register')))
 			else:
-				return redirect(url_for('StatusUpdate'))
+				return (redirect(url_for('StatusUpdate')) if next_page else redirect(url_for('StatusUpdate')))
 		else:
 			flash('Login Unsuccessful. Please check email and password', 'danger')
 	return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/register", methods=['GET', 'POST'])
+@login_required
 def Register():
 	User = mongo.db.user
 	if current_user.is_authenticated:
-		return redirect(url_for('Register'))
+		pass
 	form = RegisterForm()
-	if form.validate_on_submit():
+	if form.username.data and form.email.data and form.password.data and form.role.data is not None:
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 		user = User.find_one({"mail_id": form.email.data})
 		if user is None:
@@ -70,24 +70,32 @@ def Register():
 
 
 @app.route("/Assign",methods=['GET','POST'])
+@login_required
 def Assign():
 	if current_user.is_authenticated:
-		return redirect(url_for('Assign'))
+		pass
 	form = AssignForms()
 	User = mongo.db.user
 	Project = mongo.db.project_team
 	if form.username.data and form.project.data and form.projectteam.data is not None:
 		for us in form.username.data: 
-			teammember = User.find_one({"username" : us}, {"_id":0, "projects":0})
-			teammember['Project_Team'] = form.projectteam.data
-			Project.update({"project_name":form.project.data},{"$push":{"team_member":teammember}})
-			User.update({"username":form.username.data},{ "$push":{"projects":form.project.data}})
-			flash('Team-member has been added!','success')
+			NAU = Project.find_one({"project_name":form.project.data,"team_member.username":us})
+			if NAU is None:
+				teammember = User.find_one({"username" : us}, {"_id":0, "projects":0})
+				teammember['Project_Team'] = form.projectteam.data
+				Project.update({"project_name":form.project.data},{"$push":{"team_member":teammember}})
+				User.update({"username":us},{ "$push":{"projects":form.project.data}})
+				flash('Team-member '+us+' has been added!','success')
+				return redirect(url_for('Assign'))
+			else:
+				flash('Team-member '+us+' is already part of team!','danger')
+
 		return redirect(url_for('Assign'))
 
 	return render_template('Assign.html',title='assign',form=form)
 
 @app.route("/registerproject", methods=['GET', 'POST'])
+@login_required
 def RegisterProject():
 	form = ProjectForm()
 	Project = mongo.db.project_team
@@ -102,8 +110,71 @@ def RegisterProject():
 			flash('Projectname already exist! Enter new Projectname', 'danger')
 	return render_template('registerproject.html', title='RegisterProject', form=form)
 
+@app.route("/statusupdate", methods=['GET', 'POST'])
+@login_required
+def StatusUpdate():
+	TaskS = mongo.db.task_status
+	em = current_user.get_id()
+	form = StatusForm()
+	def usd():
+		usr = []
+		usf = list(mongo.db.user.find({'mail_id':em}))
+		for i in usf:
+			s = i['projects']
+			for e in s:
+				usr.append(e)
+		return usr
+	choices = usd()
+	usr = mongo.db.user.find_one({"mail_id":em})
+	if form.project.data and form.date.data and form.jirano.data and form.status.data and form.env.data is not None:
+		TaskS.insert({"project":form.project.data , "username":usr['username'] , "date":form.date.data , "jira_no":form.jirano.data , "desc":form.desc.data , "status":form.status.data,"env": form.env.data , "Comment":form.comments.data})
+		flash('Your status has been updated','success')
+		return redirect(url_for('StatusUpdate'))
+
+	return render_template('statusupdate.html', title='StatusUpdate', form=form,choices=choices)
+
+@app.route("/delete", methods=['GET', 'POST'])
+@login_required
+def DeleteD():
+	User = mongo.db.user
+	project = mongo.db.project_team
+	form = DeleteForms()
+	if form.project.data and form.username.data is not None:
+		project.update({'project':form.project.data},{'$pull':{'team_member':{'username':form.username.data}}})
+		User.update({'username':form.username.data},{'$pull':{'projects':form.project.data}})
+		flash('user has been removed from project','success')
+		return redirect(url_for('DeleteD'))
+	elif form.project.data is None and form.username.data is not None:
+		User.remove({"username":form.username.data})
+		flash(' user has been removed!','success')
+		return redirect(url_for('DeleteD'))
+	elif form.username.data is None and form.project.data is not None:
+		project.remove({'project':form.project.data})
+		flash(' project has been removed!','success')
+		return redirect(url_for('DeleteD'))
+
+	return render_template('delete.html', title='Delete', form=form)
+
+@app.route("/viewstatus", methods=['GET', 'POST'])
+@login_required
+def View_status():
+	form = View_statusForm()
+	if current_user.is_authenticated:
+		pass
+	else:
+		return redirect(url_for('login'))
+	stat=[]
+	Status = mongo.db.task_status
+	if form.project.data and form.username.data is not None:
+		stat = list(Status.find({"project" : form.project.data, "date" : form.date.data, "username": form.username.data}))
+		return render_template('view-status.html', form=form, stat=stat)
+	return render_template('view-status.html', form=form, stat=stat)
+
 @app.route("/logout")
+@login_required
 def logout():
 	logout_user()
 	return redirect(url_for('login'))
+
+
 
